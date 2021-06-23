3cb17595e30583e6a0e989b03c04e56517eace09
abigley1 <39389017+abigley1@users.noreply.github.com>
2021-06-23 07:26:02 -0700

    Update process_image.cpp

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -26,8 +26,8 @@ void process_image_callback(const sensor_msgs::Image img)
     
     //looping
     
-    for (int i = 0; i < img.height * img.step; i++) {
-       if (img.data[i] == white_pixel){
+    for (int i = 0; i < img.height * img.step; i=i+3) {
+       if (img.data[i] == white_pixel and img.data[i+1] == white_pixel and img.data[i+2] == white_pixel){
 
             no_ball = false;
             
